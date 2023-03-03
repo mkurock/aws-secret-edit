5f3526c26255dc723553926071fab2508e71ca96
Marvin <marvin.kurock@tech-excellence.de>
2023-03-03 09:05:36 +0100

    1.0.1

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+
+	"github.com/TylerBrock/colorjson"
+	"github.com/fatih/color"
 )
 
 type AWSResult struct {
@@ -17,64 +20,73 @@ type AWSResult struct {
 	CreatedDate   string   `json:"CreatedDate"`
 	VersionStages []string `json:"VersionStages"`
 }
+
 var TMP_FILE_NAME = ".aws-secret-tmp.json"
 
 func main() {
 	secretName := os.Args[1]
-  editor := os.Getenv("EDITOR")
-  if editor == "" {
-    editor = "vim"
-  }
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		editor = "vim"
+	}
 	fmt.Println("Secret name: ", secretName)
 	cmd := exec.Command("aws", "secretsmanager", "get-secret-value", "--secret-id", secretName)
 	var outb, errb bytes.Buffer
 	cmd.Stdout = &outb
 	cmd.Stderr = &errb
-  err := cmd.Run()
-  if err != nil {
-    fmt.Printf("%s", errb.String())
-    os.Exit(1)
-  }
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("%s", errb.String())
+		os.Exit(1)
+	}
 	var result AWSResult
 	json.Unmarshal(outb.Bytes(), &result)
 	var s interface{}
 	json.Unmarshal([]byte(result.SecretString), &s)
 	pp, _ := json.MarshalIndent(s, "", "  ")
 	os.WriteFile(TMP_FILE_NAME, pp, 0644)
-  var newSecret string
-  for {
-    cmd = exec.Command(editor, TMP_FILE_NAME)
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    err = cmd.Run()
-    if err != nil {
-      fmt.Println("Error opening editor")
-      panic(err)
-    }
-    content, err := ioutil.ReadFile(TMP_FILE_NAME)
-    if err != nil {
-      fmt.Println("Cannot read tmp file")
-    }
-    var testJson interface{}
-    err = json.Unmarshal(content, &testJson)
-    if err != nil {
-      fmt.Println("Invalid JSON:\n", err)
-      fmt.Println("----------------------------------------")
-      fmt.Println(string(content))
-      fmt.Println("----------------------------------------")
-      fmt.Println("Press enter to try again")
-      fmt.Scanln()
-      continue
-    }
+	var newSecret string
+	for {
+		cmd = exec.Command(editor, TMP_FILE_NAME)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Run()
+		if err != nil {
+			fmt.Println("Error opening editor")
+			panic(err)
+		}
+		content, err := ioutil.ReadFile(TMP_FILE_NAME)
+		if err != nil {
+			fmt.Println("Cannot read tmp file")
+		}
+		var testJson interface{}
+		err = json.Unmarshal(content, &testJson)
+		if err != nil {
+			fmt.Println("Invalid JSON:\n", err)
+			fmt.Println("----------------------------------------")
+			fmt.Println(string(content))
+			fmt.Println("----------------------------------------")
+			fmt.Println("Press enter to try again")
+			fmt.Scanln()
+			continue
+		}
 
-    newSecret = string(content)
-    os.Remove(TMP_FILE_NAME)
-    break
-  }
+		newSecret = string(content)
+		os.Remove(TMP_FILE_NAME)
+		break
+	}
 
+	f := colorjson.NewFormatter()
+	f.Indent = 2
+  f.KeyColor = color.New(color.FgBlue)
+	var newSecretObj interface{}
+	json.Unmarshal([]byte(newSecret), &newSecretObj)
+	newSecretPrettyStr, _ := f.Marshal(newSecretObj)
 	fmt.Println("New Secret value: ")
-	fmt.Println(newSecret)
+	fmt.Println("----------------------------------------")
+	fmt.Println(string(newSecretPrettyStr))
+	fmt.Println("----------------------------------------")
 	fmt.Printf("Press enter to update secret %s", secretName)
 	fmt.Scanln()
 	fmt.Println("Updating secret...")
